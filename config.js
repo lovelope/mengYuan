bf6a922646e1438b49af25b3ebfa00329a856764
lovelope <lovelope@qq.com>
2017-08-08 21:12:41 +0800

    fork from git@github.com:chenshenhai/koa2-note.git

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'mengyuan',
+    USERNAME: 'root',
+    PASSWORD: 'passph123',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
